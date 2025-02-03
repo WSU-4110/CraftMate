f4991e5e06e8e245118ffa69b135cebe3ddc0ffd
Keenan Dalati <hk3682@wayne.edu>
2025-02-02 21:13:31 -0500

    App created

---
+++ craftmate/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style= {{color: 'white', fontSize: 20}}>Welcome to CraftMate Bitches</Text>
+    </View>
+  );
+};
+
+export default App;
