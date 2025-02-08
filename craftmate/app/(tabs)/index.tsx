017b193cd6b55a4e34e9a223ac9c4fcf14282e2d
Darick Le <daricklemain@gmail.com>
2025-02-08 14:52:39 -0500

    Add logo to home page

--- craftmate/app/(tabs)/index.tsx
+++ craftmate/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const posts = [
   {
@@ -10,6 +10,26 @@ const posts = [
   {
     id: '2',
     title: 'Second Post',
+    content: 'This is the content of the second post.',
+  },
+  {
+    id: '3',
+    title: 'Third Post',
+    content: 'This is the content of the second post.',
+  },
+  {
+    id: '4',
+    title: 'Fourth Post',
+    content: 'This is the content of the second post.',
+  },
+  {
+    id: '5',
+    title: 'Fifth Post',
+    content: 'This is the content of the second post.',
+  },
+  {
+    id: '6',
+    title: 'Sixth Post',
     content: 'This is the content of the second post.',
   },
   // Add more posts as needed
@@ -31,10 +51,12 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+      <Image source={require('../../assets/images/craftmate-logo.png')} style={styles.logo} />
       <FlatList
         data={posts}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
@@ -43,8 +65,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#808080',
+    backgroundColor: '#fff',
     padding: 10,
+    alignItems: 'center', // Center the logo horizontally
+  },
+  logo: {
+    width: 260, // Adjust the width as needed
+    height: 100, // Adjust the height as needed
+    paddingTop: 50, // Add some space below the logo
+    marginBottom: 20, // Add some space below the logo
+  },
+  listContent: {
+    paddingBottom: 20, // Add some padding at the bottom of the list
   },
   postContainer: {
     backgroundColor: '#f9f9f9',
@@ -56,6 +88,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 3,
+    width: '100%', // Make sure the posts take the full width
   },
   postTitle: {
     fontSize: 18,
